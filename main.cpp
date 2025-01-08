a3f415bc942f01d830a85c40640950cb208f9b66
vbood <viktorfbood@gmail.com>
2025-01-08 19:00:57 +0100

    Remove comments for presentation

--- main.cpp
+++ main.cpp
@@ -6,9 +6,7 @@
 #include <iomanip>
 #include <sstream>
 #include <algorithm>
-//enkel draft för jämförelse mellan egen och STL binarysearch som vi visade förra veckan
-//Johann och jag håller på att få klart det sista på den andra verisonen
-//Skall kolla med Stefan ang best practice ang sort och search
+
 
 
 class BinarySearchStorage {
@@ -42,21 +40,20 @@ class BinarySearchStorage {
         }
         return false;
     }
-    // funktion för att se om sort,shuffle och generering funkar. Kan tas bort vid presentation.
-    // går även skriva en issorted funktion
-    void printFirstAndLastAccounts(){
-        std::cout << "First 3 accounts " << std::endl;
-        for (int i = 0; i < 3 && i < accounts.size(); i++)
-        {
-            std::cout << accounts[i] << std::endl;
-        }
-        std::cout << "Last 3 accounts " << std::endl;
-        for (int i = accounts.size() - 3; i < accounts.size(); i++)
-        {
-            std::cout << accounts[i] << std::endl;
-        }
+    
+    // void printFirstAndLastAccounts(){
+    //     std::cout << "First 3 accounts " << std::endl;
+    //     for (int i = 0; i < 3 && i < accounts.size(); i++)
+    //     {
+    //         std::cout << accounts[i] << std::endl;
+    //     }
+    //     std::cout << "Last 3 accounts " << std::endl;
+    //     for (int i = accounts.size() - 3; i < accounts.size(); i++)
+    //     {
+    //         std::cout << accounts[i] << std::endl;
+    //     }
         
-    }
+    // }
 
     void sortAccounts(){
         std::sort(accounts.begin(), accounts.end());
@@ -75,14 +72,14 @@ int main(){
 
     int AntalAccounts = 10000000;
 
-    //loop för att generera och padda konton med 00 och 10 tecken
+    
     for (int i = 1; i <= AntalAccounts; i++) {
         std::ostringstream oss;
         oss << std::setw(10) << std::setfill('0') << i;
         storage.addAccount(oss.str());
     }
 
-    storage.printFirstAndLastAccounts();
+    //storage.printFirstAndLastAccounts();
 
     std::string searchForAccount = "0000001337";
     std::cout << "Binary search for " << searchForAccount << std::endl;
@@ -104,19 +101,19 @@ int main(){
     std::cout << "Shuffling accounts" << std::endl;
     std::shuffle(storage.getAccounts().begin(), storage.getAccounts().end(), g);
 
-    storage.printFirstAndLastAccounts();
+    //storage.printFirstAndLastAccounts();
 
 
-    // sorterar konton och mäter tiden
+    
     std::cout << "Sorting acounts " << std::endl;
     startTime= std::chrono::high_resolution_clock::now();
     storage.sortAccounts();
     endTime = std::chrono::high_resolution_clock::now();
     std::cout << "Sorting done in "<< std::chrono::duration_cast<std::chrono::nanoseconds>(endTime    - startTime).count() << " nanoseconds" << std::endl;
 
-    storage.printFirstAndLastAccounts();
+    //storage.printFirstAndLastAccounts();
 
-    // STL algo searchen för att jämföra mot våran egen
+    
     std::cout << "Algoritmsearch for " << searchForAccount << std::endl;
     startTime= std::chrono::high_resolution_clock::now();
     bool sfound = std::binary_search(storage.getAccounts().begin(), storage.getAccounts().end(), searchForAccount);
